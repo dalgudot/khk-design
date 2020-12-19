05a05f5a8f31b84bb74be03920d87d7a7aa18008
khkim <dalgudot@gmail.com>
2020-12-19 14:56:24 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Create Next App!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -20,16 +20,15 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
+          ya
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
-
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
@@ -37,7 +36,6 @@ export default function Home() {
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
-
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
@@ -61,5 +59,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
